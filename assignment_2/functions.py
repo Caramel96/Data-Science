7dae7b0751079d7ac5e660638fca3bba7b41424f
Sam Buckby <54915156+Caramel96@users.noreply.github.com>
2021-04-25 14:57:02 +0100

    Update functions.py

--- assignment_2/functions.py
+++ assignment_2/functions.py
@@ -125,9 +125,9 @@ def encode_data(data):
 def decode_dataset(data, encoders, categorical_features):
   categorical_features = ['Type', 'Gender', 'Age range', 'Officer-defined ethnicity', 'Legislation', 'Object of search', 'Outcome', 'Force']
   df = data.copy()
-    for feat in categorical_features:
-        df[feat] = encoders[feat].inverse_transform(df[feat])
-    return df
+  for feat in categorical_features:
+    df[feat] = encoders[feat].inverse_transform(df[feat])
+  return df
 def StandardDataset_all(data):
 
   privileged_sex = np.where(categorical_names['Gender'] == 'Male')[0]
